cdb3fd7dd0453258496067b247c150f8f15032b3
The Dark Lord <rushabhhathi@gmail.com>
2015-03-21 19:50:43 +0530

    description chanaged

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'rushabhhathi:mailersinapp',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: 'Add mailer support using mailgun',
+  summary: 'Add mailer support using mailgun used currently in a private environment',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/SauronTheGreat/mailerPackage',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -18,7 +18,7 @@ Package.onUse(function(api) {
     api.use(['templating'], 'client');
     api.use('handlebars', 'client');
   api.use('underscore', 'client');
-  api.use(['gfk:mailgun-api','summernote:summernote'
+  api.use(['gfk:mailgun-api@1.0.0','summernote:summernote@0.5.10'
 ],both);
   
   api.addFiles('mailersinapp.js');
